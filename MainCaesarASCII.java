fdb81e019aa145ca812449bb482cd51841ff7b53
Zamroni Ilyas <frayzz.dmorgan@gmail.com>
2013-10-18 09:38:21 +0700

    Update MainCaesarASCII.java

--- MainCaesarASCII.java
+++ MainCaesarASCII.java
@@ -1,15 +1,6 @@
 public class MainCaesarASCII {
     public static void main(String[] args) {
         String pesan ="Haloo berrroo";
-        CaesarASCII cae = new CaesarASCII(pesan);                        
-        String enkripsi = cae.enkripsiCaesar(pesan);
-        String dekripsi = cae.dekripsiCaesar(enkripsi);
-        System.out.println("Pesan : "+pesan);
-        System.out.println("Enkripsi : "+enkripsi);
-        System.out.println("Dekripsi : "+dekripsi);
-        
-        
-        System.out.println(cae.getLog());
-        
+        CaesarASCII caesarASCII = new CaesarASCII(pesan);
     }
 }

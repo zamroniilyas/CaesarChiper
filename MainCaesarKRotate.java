a6ba715e7312c37b382c2f13a1c39e39d8cabafb
Zamroni Ilyas <frayzz.dmorgan@gmail.com>
2013-10-18 09:45:00 +0700

    Update MainCaesarKRotate.java

--- MainCaesarKRotate.java
+++ MainCaesarKRotate.java
@@ -1,15 +1,6 @@
 public class MainCaesarKRotate {
     public static void main(String[] args) {
         String pesan ="Haloo berrroo";
-        CaesarASCIIKRotate cae = new CaesarASCIIKRotate(pesan,54);                        
-        String enkripsi = cae.enkripsiCaesar(pesan);
-        String dekripsi = cae.dekripsiCaesar(enkripsi);
-        System.out.println("Pesan : "+pesan);
-        System.out.println("Enkripsi : "+enkripsi);
-        System.out.println("Dekripsi : "+dekripsi);
-        
-        
-        System.out.println(cae.getLog());
-        
+        CaesarASCIIKRotate cae = new CaesarASCIIKRotate(pesan,800);   
     }
 }
